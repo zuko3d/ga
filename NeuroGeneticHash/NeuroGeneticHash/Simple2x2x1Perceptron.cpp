b2414a4590991afdedb3611bb6cc230450df02a7
Shalyga Dmitriy <zuko3d@gmail.com>
2017-04-05 22:09:31 +0300

    Add new class

---
+++ NeuroGeneticHash/NeuroGeneticHash/Simple2x2x1Perceptron.cpp
@@ -0,0 +1,12 @@
+#include "Simple2x2x1Perceptron.h"
+
+
+
+Simple2x2x1Perceptron::Simple2x2x1Perceptron()
+{
+}
+
+
+Simple2x2x1Perceptron::~Simple2x2x1Perceptron()
+{
+}
---
+++ NeuroGeneticHash/NeuroGeneticHash/Simple2x2x1Perceptron.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Simple2x2x1Perceptron
+{
+public:
+	Simple2x2x1Perceptron();
+	~Simple2x2x1Perceptron();
+};
+
